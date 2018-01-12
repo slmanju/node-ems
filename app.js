f8f9b05f27c5577286da58158e1c74dcca5abd16
Manjula Jayawardana <manjula.jayawardana@seebo.com.au>
2018-01-12 16:11:33 +0530

    add ejs layout support

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var expressLayouts = require('express-ejs-layouts');
 
 // connecting to the database
 require("./server/configs/database");
@@ -16,9 +17,11 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'server/views'));
 app.set('view engine', 'ejs');
+app.set('layout', 'layout/main');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(expressLayouts);
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
